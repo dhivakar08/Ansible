1e90aabcf656117c407c4e8026b4982da4f6ee0e
dhiva <diwiece@gmail.com>
2020-05-05 17:59:28 +0530

    Inventory file add to git repo

--- inventory.sh
+++ inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LIST=$(aws ec2 describe-instances  --region us-west-2 --filters "Name=tag:Name,Values=ansible-node" --query "Reservations[*].Instances[*].[PrivateIpAddress]" --output text | xargs  | sed 's/[^ ][^ ]*/"&"/g' | sed -e 's/ /,/g')
+LIST=$(aws ec2 describe-instances  --region us-east-2 --filters "Name=tag:Name,Values=ansible-node" --query "Reservations[*].Instances[*].[PrivateIpAddress]" --output text | xargs  | sed 's/[^ ][^ ]*/"&"/g' | sed -e 's/ /,/g')
 echo "{
     \"group\": {
         \"hosts\": [ $LIST ]
